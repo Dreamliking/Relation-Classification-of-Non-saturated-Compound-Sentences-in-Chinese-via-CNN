6d1c3f1bc54b6daf013b58077749aba62f903e3e
Dreamliking <Dreamyan@mails.ccnu.edu.cn>
2018-03-28 11:01:49 +0800

    Create text_cnn.py

---
+++ text_cnn.py
@@ -0,0 +1,102 @@
+import tensorflow as tf
+import numpy as np
+import gensim
+
+# wordfile = './data/modify/word_embedding2.npy'
+
+# add  stop word
+wordfile = './data/word_embedding2.npy'
+
+class TextCNN(object):
+    """
+    A CNN for text classification.
+    Uses an embedding layer, followed by a convolutional, max-pooling and softmax layer.
+    """
+    def __init__(
+      self, sequence_length, num_classes, vocab_size,
+      embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0):
+
+        # Placeholders for input, output and dropout
+        self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
+        self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
+        self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
+
+        # Keeping track of l2 regularization loss (optional)
+        l2_loss = tf.constant(0.0)
+
+        # Embedding layer 嵌入层
+        # 使用预训练词向量初始化
+
+        with tf.device('/cpu:0'), tf.name_scope("embedding"):
+            # word_numpy = np.loadtxt('./data/cccs_vector.txt', dtype={'word': 'vector'},
+            #                         usecols=(range(1, embedding_size+1)))
+            # word_embedding = gensim.models.KeyedVectors.load_word2vec_format(word_numpy,
+            #                                                                  binary=False, datatype=np.float32)
+            # self.W = tf.get_variable('W', dtype=tf.float32, initializer=word_embedding[vocab_size, embedding_size])
+            embedding = np.load(Model_path).astype("float32")
+            # self.W = tf.Variable(
+            #     tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),
+            #     name="W")
+            self.W = tf.get_variable(name="embedding", initializer=embedding, dtype=tf.float32)
+            self.embedded_chars = tf.nn.embedding_lookup(self.W, self.input_x)
+            self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
+
+        # Create a convolution + maxpool layer for each filter size  卷积层 每个过滤器的池层
+        pooled_outputs = []
+        for i, filter_size in enumerate(filter_sizes):
+            with tf.name_scope("conv-maxpool-%s" % filter_size):
+                # Convolution Layer
+                filter_shape = [filter_size, embedding_size, 1, num_filters]
+                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W", dtype=tf.float32)
+                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
+                conv = tf.nn.conv2d(
+                    self.embedded_chars_expanded,
+                    W,
+                    strides=[1, 1, 1, 1],
+                    padding="VALID",
+                     name="conv")
+                # Apply nonlinearity 应用非线性的
+                h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+                # Maxpooling over the outputs
+                pooled = tf.nn.max_pool(
+                    h,
+                    ksize=[1, sequence_length - filter_size + 1, 1, 1],
+                    strides=[1, 1, 1, 1],
+                    padding='VALID',
+                    name="pool")
+                pooled_outputs.append(pooled)
+
+        # Combine all the pooled features 全连接层
+        num_filters_total = num_filters * len(filter_sizes)
+        self.h_pool = tf.concat(pooled_outputs, 3)
+        self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
+
+        # Add dropout  计算dropout
+        with tf.name_scope("dropout"):
+            self.h_drop = tf.nn.dropout(self.h_pool_flat, self.dropout_keep_prob)
+
+        # Final (unnormalized) scores and predictions 非统一化的分数和预测
+        with tf.name_scope("output"):
+            W = tf.get_variable(
+                "W",
+                shape=[num_filters_total, num_classes],
+                initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
+            l2_loss += tf.nn.l2_loss(W)
+            l2_loss += tf.nn.l2_loss(b)
+            self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
+            self.predictions = tf.argmax(self.scores, 1, name="predictions")
+
+        # CalculateMean cross-entropy loss  计算交叉熵损失
+        with tf.name_scope("loss"):
+            # 损失计算
+            losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
+            self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+
+        # Accuracy
+        with tf.name_scope("accuracy"):
+            correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
+
+
+
